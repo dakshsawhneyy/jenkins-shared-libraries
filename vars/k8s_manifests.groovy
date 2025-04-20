41ca09e73857bbf0714f060444a459f3ff35e438
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-04-21 02:00:50 +0530

    Update k8s_manifests.groovy

--- vars/k8s_manifests.groovy
+++ vars/k8s_manifests.groovy
@@ -1,26 +1,22 @@
-def k8s_manifests(String imageTag) {
+def call(String imageTag) {
     withCredentials([usernamePassword(
         credentialsId: 'github-credentials',
         usernameVariable: 'GIT_USERNAME',
         passwordVariable: 'GIT_PASSWORD'
     )]) {
         sh """
-            # Git Configuration
             git config user.name "Jenkins CI"
             git config user.email "dakshsawhney2@gmail.com"
-            
-            # Replace image tags in your app manifests
             sed -i "s|image: dakshsawhneyy/wearsphere-backend:.*|image: dakshsawhneyy/wearsphere-backend:${imageTag}|g" kubernetes/backend-deployment.yaml
             sed -i "s|image: dakshsawhneyy/wearsphere-frontend:.*|image: dakshsawhneyy/wearsphere-frontend:${imageTag}|g" kubernetes/frontend-deployment.yaml
             sed -i "s|image: dakshsawhneyy/wearsphere-admin:.*|image: dakshsawhneyy/wearsphere-admin:${imageTag}|g" kubernetes/admin-deployment.yaml
-            
-            # Check if there are changes to commit
+
             if git diff --quiet; then
                 echo "✅ No changes to commit."
             else
                 git add kubernetes/*.yaml
                 git commit -m "🔁 Update image tags to ${imageTag} [ci skip]"
-                git remote set-url origin https://\${GIT_USERNAME}:\${GIT_PASSWORD}@github.com/dakshsawhneyy/WearSphere-Ecommerce-MERN.git
+                git remote set-url origin https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/dakshsawhneyy/WearSphere-Ecommerce-MERN.git
                 git push origin HEAD:master
             fi
         """
